33f00f3a04f833d0f0fc6f1365ddc2908fdceaeb
Robin Millette <robin@millette.info>
2018-05-28 22:03:58 -0400

    Another production try (thru babel).

---
+++ was.babelrc.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+  "presets": [
+    "next/babel"
+  ],
+  "plugins": [
+    // ["transform-define", "./env-config.js"]
+    ["transform-define", { "process.env.NODE_ENV": process.env.NODE_ENV }]
+  ]
+}
