88d3b58472946711ff0cc9ad8e004cb83b286f37
Robin Millette <robin@millette.info>
2018-05-29 17:57:18 -0400

    Org form.

--- pages/orgs.js
+++ pages/orgs.js
@@ -1,41 +1,45 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { dispatch } from '@rematch/core'
 import { initStore } from '../shared/store'
 import withRematch from '../shared/utils/withRematch'
 import Header from '../shared/components/header'
+import OrgDisplay from '../shared/components/org-display'
+import OrgForm from '../shared/components/org-form'
 
 class Orgs extends Component {
+  constructor (props) {
+    super(props)
+    this.save = this.props.addAsync.bind(this)
+  }
+
   render () {
     return (
       <div>
         <Header title='Les Orgs' />
-        <h2>The count is {this.props.org.length}</h2>
-        <p>
-          <button onClick={this.props.add2}>add</button>
-          <button onClick={() => dispatch.org.add({ name: 'rah' })}>
-            add (using dispatch function)
-          </button>
-          <button onClick={this.props.add2By({ name: 'joeBy' })}>add joeBy</button>
-          <button onClick={this.props.add2Async}>incrementAsync</button>
-        </p>
+        <OrgForm username={this.props.username} save={this.save} />
+        <h3>The count is {this.props.org.length}</h3>
         <ol>
-          {this.props.org.map((o, i) => <li key={`org-${i}`}>{o.name}</li>)}
+          {this.props.org.map((x, i) => <li key={`org-${i}`}>
+            {i} {x.title}
+            <br />
+            {JSON.stringify(x)}
+          </li>)}
         </ol>
+        <hr />
+        <OrgDisplay />
       </div>
     )
   }
 }
 
 const mapState = (state) => ({
-  org: state.org
+  org: state.org,
+  username: state.auth.name
 })
 
-const mapDispatch = ({ org: { add, addAsync } }) => ({
-  add2: () => add({ name: 'joe' }),
-  add2By: (anOrg) => () => add(anOrg),
-  add2Async: () => addAsync({ name: 'joeAsync' })
+const mapDispatch = ({ org: { addAsync } }) => ({
+  addAsync: (org) => addAsync(org)
 })
 
 export default withRematch(initStore, mapState, mapDispatch)(Orgs)
--- shared/models/org.js
+++ shared/models/org.js
@@ -4,13 +4,20 @@ export default {
   state: [],
   reducers: {
     // handle state changes with pure functions
-    add: (state, payload) => [...state, payload]
+    // add: (state, payload) => [...state, payload]
+    add: (state, org) => {
+      if (!org.title) { throw new Error('Needed: title') }
+      Object.keys(org).forEach((k) => {
+        if (!org[k]) { delete org[k] }
+      })
+      return [...state, org]
+    }
   },
   effects: {
     // handle state changes with impure functions.
     // use async/await for async actions
     async addAsync (payload, rootState) {
-      await new Promise(resolve => setTimeout(resolve, 2000))
+      await new Promise(resolve => setTimeout(resolve, 200))
       this.add(payload)
     }
   }
