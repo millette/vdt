dfe642739f706370042f4456c7b4efad1afc3b32
Robin Millette <robin@millette.info>
2018-05-29 19:30:43 -0400

    Volunteer form.

--- pages/volunteers.js
+++ pages/volunteers.js
@@ -1,42 +1,45 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { dispatch } from '@rematch/core'
 import { initStore } from '../shared/store'
 import withRematch from '../shared/utils/withRematch'
 import Header from '../shared/components/header'
-
-const birthdate = '1971-11-22'
+import OrgDisplay from '../shared/components/org-display'
+import VolunteerForm from '../shared/components/volunteer-form'
 
 class Volunteers extends Component {
+  constructor (props) {
+    super(props)
+    this.save = this.props.addVolunteerAsync.bind(this)
+  }
+
   render () {
     return (
       <div>
-        <Header />
-        <h1>Counter</h1>
-        <h2>{this.props.volunteers.length}</h2>
-        <h3>{(this.props.volunteers.length && this.props.volunteers[this.props.volunteers.length - 1].name) || ''}</h3>
-        <h3>{(this.props.volunteers.length && this.props.volunteers[this.props.volunteers.length - 1].birthdate) || ''}</h3>
-        <p>
-          <button onClick={this.props.addVolunteer}>increment</button>
-          <button onClick={() => dispatch.volunteer.addVolunteer({ birthdate, name: 'Dis' })}>
-            increment (using dispatch function)
-          </button>
-          <button onClick={this.props.addVolunteerBy({ name: 'Rah' })}>increment by 5</button>
-          <button onClick={this.props.addVolunteerAsync}>incrementAsync</button>
-        </p>
-        <br />
+        <Header title='Les volontaires' />
+        <VolunteerForm username={this.props.username} save={this.save} />
+        <h3>The count is {this.props.volunteers.length}</h3>
+        <ol>
+          {this.props.volunteers.map((x, i) => <li key={`volunteer-${i}`}>
+            {i} {x.title}
+            <br />
+            {JSON.stringify(x)}
+          </li>)}
+        </ol>
+        <hr />
+        <OrgDisplay />
       </div>
     )
   }
 }
 
-const mapState = (state) => ({ volunteers: state.volunteer })
+const mapState = (state) => ({
+  volunteers: state.volunteer,
+  username: state.auth.name
+})
 
-const mapDispatch = ({ volunteer: { addVolunteer, addVolunteerAsync } }) => ({
-  addVolunteer: () => addVolunteer({ birthdate, name: 'Joe' }),
-  addVolunteerBy: (o) => () => addVolunteer({ ...o, birthdate: '2001-05-10' }),
-  addVolunteerAsync: () => addVolunteerAsync({ birthdate, name: 'Bomp' })
+const mapDispatch = ({ volunteer: { addVolunteerAsync } }) => ({
+  addVolunteerAsync: (volunteer) => addVolunteerAsync(volunteer)
 })
 
 export default withRematch(initStore, mapState, mapDispatch)(Volunteers)
--- shared/models/volunteer.js
+++ shared/models/volunteer.js
@@ -3,12 +3,19 @@
 export default {
   state: [],
   reducers: {
-    addVolunteer: (state, volunteer) => [...state, volunteer]
+    // addVolunteer: (state, volunteer) => [...state, volunteer]
+    add: (state, volunteer) => {
+      if (!volunteer.title) { throw new Error('Needed: title') }
+      Object.keys(volunteer).forEach((k) => {
+        if (!volunteer[k]) { delete volunteer[k] }
+      })
+      return [...state, volunteer]
+    }
   },
   effects: {
     async addVolunteerAsync (volunteer, rootState) {
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      this.addVolunteer(volunteer)
+      await new Promise(resolve => setTimeout(resolve, 100))
+      this.add(volunteer)
     }
   }
 }
