0f36449fad3a4996412c2faa84668e6dd8e7b88a
Robin Millette <robin@millette.info>
2018-05-29 17:57:43 -0400

    Babel config attempt #n.

--- was.babelrc.js
+++ was.babelrc.js
@@ -1,11 +1,11 @@
 'use strict'
 
 module.exports = {
-  "presets": [
-    "next/babel"
+  presets: [
+    'next/babel'
   ],
-  "plugins": [
+  plugins: [
     // ["transform-define", "./env-config.js"]
-    ["transform-define", { "process.env.NODE_ENV": process.env.NODE_ENV }]
+    ['transform-define', { 'process.env.NODE_ENV': process.env.NODE_ENV }]
   ]
 }
