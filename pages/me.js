90abe4379256dbd309cf6a16245300b2411bac77
Robin Millette <robin@millette.info>
2018-06-05 17:04:40 -0400

    Refactor me page a bit.

--- pages/me.js
+++ pages/me.js
@@ -7,43 +7,41 @@ import withRematch from '../shared/utils/withRematch'
 import Droits from '../shared/components/droits'
 import Summary from '../shared/components/summary'
 
+const KnownUser = (props) => {
+  if (props.usertype) {
+    if (typeof props.userpos === 'number') {
+      // fully known
+      return <p>Vous êtes le {props.usertype} #{props.userpos}.</p>
+    }
+    return <p>Vous êtes un {props.usertype} inconnu.</p>
+  }
+
+  return <p>
+    Quel type d’utilisateur?
+    <span className='field is-grouped'>
+      <span className='control'>
+        <button onClick={props.type.bind(props, 'donor')} className='button'>Donateur</button>
+      </span>
+      <span className='control'>
+        <button onClick={props.type.bind(props, 'org')} className='button'>Org</button>
+      </span>
+      <span className='control'>
+        <button onClick={props.type.bind(props, 'volunteer')} className='button'>Volontaire</button>
+      </span>
+    </span>
+  </p>
+}
+
 const Me = (props) => {
   let out
   let title
   if (props.username) {
-    let knownUser
-
-    if (props.usertype) {
-      if (typeof props.userpos === 'number') {
-        // fully known
-        knownUser = <p>Vous êtes le {props.usertype} #{props.userpos}.</p>
-      } else {
-        // known type
-        knownUser = <p>Vous êtes un {props.usertype} inconnu.</p>
-      }
-    } else {
-      // who that?
-      knownUser = <p>
-        Quel type d’utilisateur?
-        <span className='field is-grouped'>
-          <span className='control'>
-            <button onClick={props.type.bind(props, 'donor')} className='button'>Donateur</button>
-          </span>
-          <span className='control'>
-            <button onClick={props.type.bind(props, 'org')} className='button'>Org</button>
-          </span>
-          <span className='control'>
-            <button onClick={props.type.bind(props, 'volunteer')} className='button'>Volontaire</button>
-          </span>
-        </span>
-      </p>
-    }
     title = 'Mon profile: ' + props.username
     out = <div className='content'>
       <p>Salut {props.username}.</p>
       <Summary />
       <Droits />
-      {knownUser}
+      <KnownUser type={props.type} usertype={props.usertype} userpos={props.userpos} />
     </div>
   } else {
     title = 'Please login'
