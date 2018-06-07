ea2b6851a9fdb1f39f1eec1460ecf2637b93c032
Robin Millette <robin@millette.info>
2018-06-06 23:08:39 -0400

    Bah

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,6 @@
 // _document is only rendered on the server side and not on the client side
 // Event handlers like onClick can't be added to this file
 
-// ./pages/_document.js
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
