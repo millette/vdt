5ecf694bdf82a906a692da9a0c85a0d868227896
Robin Millette <robin@millette.info>
2018-06-08 07:18:11 -0400

    Navbar toggler.

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,6 @@ export default class MyDocument extends Document {
             </div>
           </footer>
           <NextScript />
-          <script src='/static/js/nav.js' />
         </body>
       </html>
     )
--- shared/components/main-menu.js
+++ shared/components/main-menu.js
@@ -1,27 +1,43 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'next/link'
 
-const MainMenu = () => <nav className='navbar is-primary' role='navigation' aria-label='main navigation'>
-  <div className='container'>
-    <div className='navbar-brand'>
-      <Link href='/' prefetch><a className='navbar-item'>Accueil</a></Link>
-      <button role='button' data-target='navMenu' className='navbar-burger' aria-label='menu' aria-expanded='false'>
-        <span aria-hidden />
-        <span aria-hidden />
-        <span aria-hidden />
-      </button>
-    </div>
-    <div className='navbar-menu' id='navMenu'>
-      <div className='navbar-start'>
-        <Link href='/gifts' prefetch><a className='navbar-item'>Magasin</a></Link>
+class MainMenu extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { active: false }
+  }
+
+  toggle (ev, b) {
+    // // console.log('REF', this.refs.navMenu)
+    // console.log('TOGGLE', ev.target)
+    // console.log('ACTIVE', this.state.active)
+    this.setState({ active: !this.state.active })
+  }
+
+  render () {
+    return <nav className='navbar is-primary' role='navigation' aria-label='main navigation'>
+      <div className='container'>
+        <div className='navbar-brand'>
+          <Link href='/' prefetch><a className='navbar-item'>Accueil</a></Link>
+          <button role='button' onClick={this.toggle.bind(this)} className={`navbar-burger${this.state.active ? ' is-active' : ''}`} aria-label='menu' aria-expanded='false'>
+            <span aria-hidden />
+            <span aria-hidden />
+            <span aria-hidden />
+          </button>
+        </div>
+        <div className={`navbar-menu${this.state.active ? ' is-active' : ''}`}>
+          <div className='navbar-start'>
+            <Link href='/gifts' prefetch><a className='navbar-item'>Magasin</a></Link>
+          </div>
+          <div className='navbar-end'>
+            <Link href='/admin' prefetch><a className='navbar-item'>Admin</a></Link>
+          </div>
+        </div>
       </div>
-      <div className='navbar-end'>
-        <Link href='/admin' prefetch><a className='navbar-item'>Admin</a></Link>
-      </div>
-    </div>
-  </div>
-</nav>
+    </nav>
+  }
+}
 
 export default MainMenu
