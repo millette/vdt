5e9340939f55029ec987bde1170213c442487329
Robin Millette <robin@millette.info>
2018-06-08 17:39:27 -0400

    Remove unused config files.

--- env-config.js
+++
@@ -1,6 +0,0 @@
-// const prod = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // 'process.env.BACKEND_URL': prod ? '/Next-gh-page-example' : ''
-  'process.env.NODE_ENV': process.env.NODE_ENV
-}
