5a0166c2519af5212eb3338caf6958d41df0a7ab
Robin Millette <robin@millette.info>
2018-06-21 14:49:09 -0400

    Link to source tree, not commit.

--- shared/components/build-link.js
+++ shared/components/build-link.js
@@ -4,7 +4,7 @@ import React, { Fragment } from 'react'
 
 const BuildLink = ({ buildId }) => {
   if (buildId === '-') { return <i>dev</i> }
-  const buildUrl = 'https://github.com/millette/vdt/commit/' + buildId
+  const buildUrl = 'https://github.com/millette/vdt/tree/' + buildId
 
   return <Fragment>
     commit: <a target='_blank' href={buildUrl}>{buildId}</a>
